ec629def993a4306a7480d859209f6c211ea536e
joe <joe>
2023-02-05 20:07:14 +0100

    refacto folder

--- project/app/main.py
+++ project/app/main.py
@@ -1,13 +1,7 @@
-import dotenv
 from fastapi import FastAPI, Depends
-
 from app.config import get_settings, Settings
 
-
-dotenv.load_dotenv('.env')
-
 app = FastAPI()
-
 
 @app.get('/ping')
 async def pong(settings: Settings = Depends(get_settings)):
